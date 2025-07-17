0fdfbdafb1d6646d8c62b4d62f7f1499d9073aec
Fadillatur Rizki <fadillaturrizki@gmail.com>
2025-07-17 21:39:47 +0000

    taro di script.js aja dan sesuaikan codenya agar saya mudah edit di repo

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,10 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [
+    googleAI({
+      apiKey: "PASTE_YOUR_API_KEY_HERE", // PERINGATAN: Tidak disarankan menaruh API key di sini.
+    }),
+  ],
   model: 'googleai/gemini-1.5-flash',
 });

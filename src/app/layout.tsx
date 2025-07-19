0f00c69ac8a0501a0f9298ced2b1234e73e6e47a
Fadillatur Rizki <fadillaturrizki@gmail.com>
2025-07-19 05:42:07 +0000

    hilangkan Menambahkan Efek Aurora

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,12 +20,6 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased relative">
-        <div className="absolute top-0 left-0 -z-10 h-96 w-full bg-background">
-          <div className="absolute inset-0 -z-10 h-full w-full bg-background [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-          <div className="absolute top-1/2 left-1/2 -z-10 -translate-x-1/2 -translate-y-1/2">
-            <div className="h-[32rem] w-[32rem] rounded-full bg-[radial-gradient(circle_at_center,_var(--tw-gradient-stops))] from-primary/20 via-primary/10 to-transparent blur-3xl"></div>
-          </div>
-        </div>
         {children}
         <Toaster />
       </body>
